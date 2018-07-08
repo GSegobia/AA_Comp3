fdf33c42107b1875ae0cdba96ebc391d9a9718b1
Fellipe Bravo <fellipe.bravo@gmail.com>
2018-07-08 09:37:59 -0300

    Adicionada lib (IntelliJ) p/ facilitar inicialização do tomcat

--- SISFARJ/src/main/java/dominio/DiretorTecnico.java
+++ SISFARJ/src/main/java/dominio/DiretorTecnico.java
@@ -1,18 +1,18 @@
-    package dominio;
+package dominio;
 
-    /**
-     * Created by over on 05/07/18.
-     */
-    public class DiretorTecnico extends Usuario {
+/**
+ * Created by over on 05/07/18.
+ */
+public class DiretorTecnico extends Usuario {
 
-        public DiretorTecnico(int id,String nome,String email, String senha){
-            super(id,nome,email,senha,1);
-        }
-
-        public void listarCompeticoes() {}
-        public void selecionarCompeticao() {}
-        public void selecionarProva() {}
-        public void informarTempoProva() {}
-        public void informarTerminoLancamentoTempoDaProva() {}
-
+    public DiretorTecnico(int id,String nome,String email, String senha){
+        super(id,nome,email,senha,1);
     }
+
+    public void listarCompeticoes() {}
+    public void selecionarCompeticao() {}
+    public void selecionarProva() {}
+    public void informarTempoProva() {}
+    public void informarTerminoLancamentoTempoDaProva() {}
+
+}
