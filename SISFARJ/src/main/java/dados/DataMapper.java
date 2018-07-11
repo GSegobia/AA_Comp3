014be43b71f71d7b3b26b652ad8d914e066dbd97
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2018-07-11 18:45:30 -0300

    Data Mapper

--- SISFARJ/src/main/java/dados/DataMapper.java
+++ SISFARJ/src/main/java/dados/DataMapper.java
@@ -1,18 +1,16 @@
 package dados;
 
+import exceptions.ModeloNaoExiste;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-/**
- * Created by over on 06/07/18.
- */
-public interface DataMapper<Model> {
-    Model mapModel(ResultSet rs) throws ClassNotFoundException, SQLException;
-    boolean exists(int id) throws ClassNotFoundException, SQLException;
-    Model get(int id) throws ClassNotFoundException, SQLException;
-    boolean create(Model modelo) throws ClassNotFoundException, SQLException;
-    boolean update(Model modelo) throws ClassNotFoundException, SQLException;
-    ArrayList<Model> findAll() throws ClassNotFoundException, SQLException;
-
+public abstract class DataMapper<Model> {
+    public abstract Model mapModel(ResultSet rs) throws ClassNotFoundException, SQLException;
+    public abstract boolean exists(int id) throws ClassNotFoundException, SQLException;
+    public abstract Model get(int id) throws ClassNotFoundException, SQLException, ModeloNaoExiste;
+    public abstract boolean create(Model modelo) throws ClassNotFoundException, SQLException;
+    public abstract boolean update(Model modelo) throws ClassNotFoundException, SQLException;
+    public abstract ArrayList<Model> findAll() throws ClassNotFoundException, SQLException;
 }
