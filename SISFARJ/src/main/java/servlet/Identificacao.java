5ac147a8f1e3f5071fe1bf199bb852eb6f863d1d
Fellipe Bravo <fellipe.bravo@gmail.com>
2018-07-14 23:06:43 -0300

    Correção de referenciamento p/ todos os servlets

--- SISFARJ/src/main/java/servlet/Identificacao.java
+++ SISFARJ/src/main/java/servlet/Identificacao.java
@@ -10,7 +10,10 @@ import java.io.IOException;
  */
 public interface Identificacao {
     default void validarIdentidade(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("referencia", req.getRequestURI());
+        String uri = req.getRequestURI();
+        String queryString = req.getQueryString();
+        String referencia = queryString == null ? uri : String.format("%s?%s", uri, queryString);
+        req.setAttribute("referencia", referencia);
         req.getServletContext().getRequestDispatcher("/identificar.jsp").forward(req, resp);
     }
 }
